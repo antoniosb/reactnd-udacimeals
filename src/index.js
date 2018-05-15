633c2ffdaf72fd840ddc6f98f56c486b92ce257b
Antônio Augusto <antonioabritto@gmail.com>
2018-05-15 02:27:22 -0300

    create a store with redux-dev-tools

--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
+import calendar from './reducers';
+
+const store = createStore(
+  calendar,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
